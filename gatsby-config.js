b3b686c138730d306b2430487967303b92bafba1
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-05-13 15:31:35 -0400

    Add gatsby-source-filestyle dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
